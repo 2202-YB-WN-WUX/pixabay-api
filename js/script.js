16eb768f984faa0766ebf2ff056f310d4deba174
Tim Stannard <tim.stannard@yoobeecolleges.com>
2022-07-26 13:44:56 +1200

    added filtering

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-const apiKey = "your-api-key";
+// declare our settings
+const apiKey = "23357829-9b7269d284a8f7fd567a2b936";
 const endpointURL = "https://pixabay.com/api/?key=";
+// const parameters = "&q=cute+dog";
+
+// declare our elements
 const result = document.getElementById("result");
+const searchInput = document.getElementById("search-terms");
+const goBtn = document.getElementById("go");
 
+// showing images
 let showImages = (images) => {
-
+    result.innerHTML = "";
     // this function renders the image on the page
     let renderImage = (item, index) => {
+        // here's a function which checks if the user image exists
+        // if it doesn't then we'll return a generic profile image
+        let checkIfImageExists = () => {
+            console.log(item.userImageURL);
+            if (item.userImageURL == "") {
+                return "https://icon-library.com/images/white-profile-icon/white-profile-icon-24.jpg";
+            } else {
+                return item.userImageURL;
+            }
+        }
         // console.log(item.largeImageURL);
         result.innerHTML += `
         <div class="image">
         <img class="big-img" src="${item.largeImageURL}" alt="pixa image">
         <p>
-        <img class="avatar" src="${item.userImageURL}">
+        <img class="avatar" src="${checkIfImageExists()}">
         <span class="username">${item.user}</span>
         <br> 
         <i class="bi bi-heart-fill"></i> ${item.likes}</p>
@@ -23,22 +40,42 @@ let showImages = (images) => {
     images.forEach(renderImage);
 }
 
-$.ajax({
-    type: "GET",
-    url: endpointURL + apiKey,
-    success: (data) => {
-        console.log(data.hits);
-        showImages(data.hits);
-    },
-    error: (error) => {
-        console.log("there's a problem");
-        console.log(error);
-    }
-});
+// $.ajax({
+//     type: "GET",
+//     url: endpointURL + apiKey + parameters,
+//     success: (data) => {
+//         console.log(data.hits);
+//         showImages(data.hits);
+//     },
+//     error: (error) => {
+//         console.log("there's a problem");
+//         console.log(error);
+//     }
+// });
+
+goBtn.onclick = () => {
+    searchString = searchInput.value;
+    color = document.getElementById("color").value;
+    $.ajax({
+        type: "GET",
+        url: endpointURL + apiKey + "&q=" + searchString + "&colors=" + color,
+        success: (data) => {
+            console.log(data.hits);
+            showImages(data.hits);
+        },
+        error: (error) => {
+            console.log("there's a problem");
+            console.log(error);
+        }
+    });
+}
 
 // Activity:
-// Show the hits (photos) using a forEach
 
-// Activity:
-// Show the user name who uploaded it.
-// Show the amount of likes with a heart icon next to it.
+// - Create a dropdown which lets us sort via popularity/latest
+
+// - Create a dropdown which lets us choose the image_type
+
+// e.g."all", "photo", "illustration", "vector"
+
+// Color filter e.g. greyscale
